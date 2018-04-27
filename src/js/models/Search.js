a9af88b0725f6baffa0538907e947cf04155ba72
Drew Hollenback <drewsee26@gmail.com>
2018-04-26 20:21:19 -0700

    added call to api for recipe data and various methods

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
+import { key, proxy } from '../config'
 
 export default class Search {
     constructor(query) {
         this.query = query
     }
     async getResults() {
-        const key = '987bae9594ba77f27610e5ac5ab6af5f'
-        const proxy = 'https://cors-anywhere.herokuapp.com/'
+
         try {
             const result = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`)
             this.result = result.data.recipes
